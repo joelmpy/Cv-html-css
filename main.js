ea7f0d6f7213a7f8660d841cc8f8acfb82c1a5e7
joel <joel_mpy@hotmail.com>
2023-01-25 18:06:42 +0100

    responsive

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+// Je recupère le body et la div toggle ou il y a mes boutton en display none//
+
+
 var toggle = document.querySelector(".toggle")
 let body = document.querySelector("body")
+
+// Ensuite je fais un évenement pour que au click le toggle ce declanche et mes links apparaise ou pas //
 
 toggle.addEventListener("click", function () {
     body.classList.toggle('open')
